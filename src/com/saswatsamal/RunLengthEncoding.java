491320abb46d6060821c4f739440c45fdea67b71
saswatsamal <samalsaswat0@gmail.com>
2021-05-09 14:28:52 +0530

    RunLengthEncoding

---
+++ src/com/saswatsamal/RunLengthEncoding.java
@@ -0,0 +1,31 @@
+package com.saswatsamal;
+
+public class RunLengthEncoding {
+    public String encoding(String toEncode)
+    {
+        int consecutiveCharCount = 1;
+        StringBuilder encodedString = new StringBuilder();
+        //travsersing the entire string
+        for(int currentChar = 1; currentChar <= toEncode.length(); ++currentChar){
+            if(currentChar == toEncode.length() || toEncode.charAt(currentChar) != toEncode.charAt(currentChar-1))
+            {
+                encodedString.append(consecutiveCharCount);
+                encodedString.append(toEncode.charAt(currentChar-1));
+                consecutiveCharCount = 1; //resetting it to one
+            }
+            else
+            {
+                ++consecutiveCharCount;
+            }
+        }
+        return encodedString.toString();
+    }
+
+    public static void main(String[] args) {
+        RunLengthEncoding runLengthEncoding = new RunLengthEncoding();
+        String testCase1 = "AAAABBBBCDDDD";
+        System.out.println("The RLE of "+testCase1+" : "+runLengthEncoding.encoding(testCase1));
+        String testCase2 = "WWBBLLLOOPPQQ";
+        System.out.println("The RLE of "+testCase2+" : "+runLengthEncoding.encoding(testCase2));
+    }
+}
